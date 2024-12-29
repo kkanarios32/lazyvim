6ef185b22a28ceb6627541e992da5e7e778afc37
Kellen <kanarioskellen@gmail.com>
2024-12-28 21:59:55 -0500

    anki stuff

--- lua/plugins/anki.lua
+++ lua/plugins/anki.lua
@@ -5,12 +5,9 @@ return {
     opts = {
       {
         -- this function will add support for associating '.anki' extension with both 'anki' and 'tex' filetype.
-        tex_support = true,
         models = {
           -- Here you specify which notetype should be associated with which deck
-          NoteType = "PathToDeck",
           ["Basic"] = "Math",
-          ["Super Basic"] = "Deck::ChildDeck",
         },
       },
     },
